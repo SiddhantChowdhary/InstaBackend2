1b628632651ab339501f21d233c9c5d9b9f1a24c
SiddhantChowdhary <suraj5feb@gmail.com>
2024-07-30 14:19:28 +0530

    pub

---
+++ src/main/java/com/cp/instabackend2/InstaBackend2Application.java
@@ -0,0 +1,13 @@
+package com.cp.instabackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cp/instabackend2/InstaBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.instabackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
